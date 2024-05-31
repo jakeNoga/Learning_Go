691d775823acd94a0e6a8d5ff8cb0f10d7b555e5
Jake Noga <jacob.noga@hpe.com>
2024-05-31 14:59:54 -0500

    Finished example https://go.dev/doc/code

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,20 @@
+/*
+ * Author: Jake Noga
+ * Date: 5/31/2024
+ *
+ * This example was found and followed from: https://go.dev/doc/code
+ */
+
 package main
 
-import "fmt"
+import (
+	"example/user/hello/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
